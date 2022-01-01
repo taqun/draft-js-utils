31c7f6c85c4a806076c3ada4ebc0678cfd452ca4
taqun <694558+taqun@users.noreply.github.com>
2022-01-02 01:56:16 +0900

    Modify

--- packages/draft-js-export-markdown/src/stateToMarkdown.js
+++ packages/draft-js-export-markdown/src/stateToMarkdown.js
@@ -139,7 +139,7 @@ class MarkupGenerator {
           }
         }
         let indent = ' '.repeat(block.depth * 4);
-        let bracket = block.getData().get('checked') ? '- [x]' : '- [ ]';
+        let bracket = block.getData().get('checked') ? '- [x] ' : '- [ ] ';
         this.output.push(indent + bracket + this.renderBlockContent(block) + '\n');
         break;
       }
